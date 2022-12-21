0d59b64700ed6e769541b57e0f7f9e2eb67eb7fc
zBarba <ocram203@gmail.com>
2022-12-21 21:59:29 +0100

    /----------\ |  INIZIO  | \----------/
    Da questo commit in poi possiamo iniziare a diramarci ognuno per i conti propri.
    
    il makefile produce un eseguibile "game" compilando "main.cpp"
    
    visto che non succede letteralmente niente nel main, è normale che vengano segnati migliaia di FPS

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <ncurses.h>
+#include <chrono>
+using namespace std ;
+
+long int deltaTime ; // durata in nanosecondi di ogni "ciclo" del gioco
+
+int main(){
+	initscr() ;
+	deltaTime = 0 ;
+
+	while(true){
+		//carica il livello
+
+		while(true){
+			std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now() ; //tick
+
+			//  INPUT
+
+
+			//  calcoli
+
+
+			double seconds = deltaTime/(double)1000000000 ; //da nanosecondi a secondi
+			double fps = 1/seconds ;
+
+			//  OUTPUT
+			move(0, 1) ;
+			printw("-fps: %.0f ", fps) ;
+			printw("-seconds: %f ", seconds) ;//quanti secondi dura un ciclo
+			refresh() ;
+
+			std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now() ; //tock
+			deltaTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() ;
+			// milli > micro > nano
+			// deltaTime viene calcolato in nanosecondi -> 1*10^-9 secondi
+		}
+	}
+	endwin() ;
+}

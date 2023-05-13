cfaaeeb6e533c898f97a7af4af6bf283f6e02c20
zBarba <ocram203@gmail.com>
2023-05-13 11:55:14 +0200

    praticamente finita classe yuck. da testare

--- yuck.cpp
+++ yuck.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 yuck::yuck(int x, int y, level* lM, bulletManager* bM, int h, double fr, int dm): shooter(x,y,lM,bM,h,fr,dm){
+	this->chargeTime = chargeTime;
+	fireRate = 0.1;
+	chargeTime = 5; // 5 secondi
+	laserTime = 3; // 3 secondi
+	t = 0;
+	shooting = false;
+
 	box.a.x -= 1;
 	box.b.x += 1;
 	box.a.y -= 1;
@@ -10,7 +17,11 @@ yuck::yuck(int x, int y, level* lM, bulletManager* bM, int h, double fr, int dm)
 yuck::yuck(int x, int y, level* lM, bulletManager* bM): shooter(x,y,lM,bM){
 	damage *= 4;
 	health *= 4;
-	fireRate *= 0.8;
+	fireRate = 0.1;
+	chargeTime = 5; // 5 secondi
+	laserTime = 3; // 3 secondi
+	t = 0;
+	shooting = false;
 
 	box.a.x -= 1;
 	box.b.x += 1;
@@ -20,6 +31,23 @@ yuck::yuck(int x, int y, level* lM, bulletManager* bM): shooter(x,y,lM,bM){
 void yuck::update(point target, timeSpan deltaTime){
 	if(awake){
 		shooter::update(target, deltaTime);
+		t += deltaTime;
+
+		if(shooting && t>laserTime){
+			shooting = false;
+			t = 0;
+		}else if (!shooting && t>chargeTime){
+			shooting = true;
+			t = 0;
+		}else if (shooting && t<laserTime)
+			if( lastShot > fireRate ){
+				shoot();
+				lastShot = 0;
+			}else{
+				lastShot += deltaTime;
+			}
+		}
+
 	}else{
 		bM->check(box);					// faccio un bM->check prima senza salvare i danni
 		entity::update(deltaTime);		// perchè quando dormiente yuck è immortale come un sasso
@@ -49,8 +77,21 @@ void yuck::print(timeSpan deltaTime){
 	attrset(COLOR_PAIR(1));
 }
 
-void yuck::shoot(point p){
-	//se il laser è carico, inizia a sparare.
-	//il laser dura tot poi si ferma e riinizia a caricarsi
-	//implementato come tantissimi proiettili
+void yuck::shoot(){
+	vector speed;
+	speed.x = 400;
+	speed.y = 0;
+	if( !facingRight ) speed.x *= -1;
+
+	point muzzle;
+	muzzle.x = box.a.x+2;
+	muzzle.y = box.a.y+2;
+	if( facingRight ) muzzle.x += 3;
+	else muzzle.x -= 3;
+
+	bM->add(muzzle, speed, false, damage, '=');
+}
+
+void yuck::wakeUp(){
+	awake = true;
 }
--- yuck.hpp
+++ yuck.hpp
@@ -5,13 +5,18 @@
 
 class yuck: public shooter{
 protected:
-	bool awake
+	bool awake;
+	timeSpan chargeTime; // si può fare const?
+	timeSpan laserTime; // si può fare const?
+	timeSpan t; // conta il tempo
+	bool shooting;
 public:
 	yuck(int x, int y, level* lM, bulletManager* bM, int h, double fr, int dm);
 	yuck(int x, int y, level* lM, bulletManager* bM);
-	void update(point target, timeSpan deltaTime)
+	void update(point target, timeSpan deltaTime);
 	void print(timeSpan deltaTime);
-	void shoot(point p);
+	void shoot();
+	void wakeUp();
 };
 
 #endif //YUCK_HPP
